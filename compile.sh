06db188d1cbe39bc7c4aa5c1b818f0259e5bb83d
echabert <Eric.Chabert@cern.ch>
2023-03-31 23:41:35 +0200

    add c++11

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
-g++ -fPIC `root-config --cflags --glibs` -c src/RegionMassPlot.cc 
-g++ -fPIC `root-config --cflags --glibs` -c src/PlotTools.cc 
-g++ -fPIC `root-config --cflags --glibs` -c src/MassTools.cc 
-g++ -fPIC `root-config --cflags --glibs` -c src/MassTools.cc 
+g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/RegionMassPlot.cc 
+g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/PlotTools.cc 
+g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/MassTools.cc 
 g++ -fPIC -shared MassTools.o PlotTools.o RegionMassPlot.o -o libTools.so
