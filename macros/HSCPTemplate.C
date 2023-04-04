485e01a6bbd4e36a88f92e2b5e063ae84f9dbd3a
echabert <Eric.Chabert@cern.ch>
2023-04-04 18:22:34 +0200

    add new files in macros

---
+++ macros/HSCPTemplate.C
@@ -0,0 +1,180 @@
+#define HSCPSelector_cxx
+// The class definition in HSCPSelector.h has been generated automatically
+// by the ROOT utility TTree::MakeSelector(). This class is derived
+// from the ROOT class TSelector. For more information on the TSelector
+// framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
+
+
+// The following methods are defined in this file:
+//    Begin():        called every time a loop on the tree starts,
+//                    a convenient place to create your histograms.
+//    SlaveBegin():   called after Begin(), when on PROOF called only on the
+//                    slave servers.
+//    Process():      called for each event, in this function you decide what
+//                    to read and fill your histograms.
+//    SlaveTerminate: called at the end of the loop on the tree, when on PROOF
+//                    called only on the slave servers.
+//    Terminate():    called at the end of the loop on the tree,
+//                    a convenient place to draw/fit your histograms.
+//
+// To use this file, try the following session on your Tree T:
+//
+// root> T->Process("HSCPSelector.C")
+// root> T->Process("HSCPSelector.C","some options")
+// root> T->Process("HSCPSelector.C+")
+//
+
+float K(2.30), C(3.17); //Data
+float K_data2018(2.27), C_data2018(3.16); //Data 2018
+float K_data2017(2.30), C_data2017(3.17);
+
+#include "HSCPSelector.h"
+#include <TH2.h>
+#include <TStyle.h>
+
+//ADD-SELECTION-METHODS
+bool HSCPSelector::PassPreselection(int hscpIndex){
+  if (hscpIndex<0 || hscpIndex>(int)Pt.GetSize()) return false;
+  return passPreselection->at(hscpIndex);
+}
+bool HSCPSelector::PassPreselectionSept8(int hscpIndex){
+  if (hscpIndex<0 || hscpIndex>(int)Pt.GetSize()) return false;
+  return passPreselectionSept8[hscpIndex];
+}
+
+void HSCPSelector::Begin(TTree * /*tree*/)
+{
+   // The Begin() function is called at the start of the query.
+   // When running with PROOF Begin() is only called on the client.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+   TString option = GetOption();
+   //Add selections into a vector - to be updated
+   //FILL-SELECTION-VECTOR
+   selections_.push_back(&HSCPSelector::PassPreselection);
+   selections_.push_back(&HSCPSelector::PassPreselectionSept8);
+   selLabels_.push_back("PassPreselection");
+   selLabels_.push_back("PassPreselectionSept8");
+}
+
+void HSCPSelector::SlaveBegin(TTree * /*tree*/)
+{
+   // The SlaveBegin() function is called after the Begin() function.
+   // When running with PROOF SlaveBegin() is called on each slave server.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+   //create all objects that will be used by the methods Process
+   
+   //Add selections into a vector - to be updated
+   //FILL-SELECTION-VECTOR
+   selections_.push_back(&HSCPSelector::PassPreselection);
+   selections_.push_back(&HSCPSelector::PassPreselectionSept8);
+   selLabels_.push_back("PassPreselection");
+   selLabels_.push_back("PassPreselectionSept8");
+   //create RegionMassPlot for all selection
+   cout<<"HERE "<<selLabels_.size()<<" "<<selections_.size()<<endl;
+   mrp = new RegionMassPlot("toto",50,50,50,50);
+   for(unsigned int i=0;i<selLabels_.size();i++){
+      cout<<"create"<<endl;
+      RegionMassPlot reg(selLabels_[i],50,50,50,50);
+      cout<<"done"<<endl;
+      vmrp.push_back(std::move(reg));
+      cout<<"loop"<<endl;
+   }
+   
+   cout<<"THERE"<<endl;
+   //fOutput->Add(fout);
+   //mrp->addToList(fOutput);
+}
+
+Bool_t HSCPSelector::Process(Long64_t entry)
+{
+   // The Process() function is called for each entry in the tree (or possibly
+   // keyed object in the case of PROOF) to be processed. The entry argument
+   // specifies which entry in the currently loaded tree is to be processed.
+   // When processing keyed objects with PROOF, the object is already loaded
+   // and is available via the fObject pointer.
+   //
+   // This function should contain the \"body\" of the analysis. It can contain
+   // simple or elaborate selection criteria, run algorithms on the data
+   // of the event and typically fill histograms.
+   //
+   // The processing can be stopped by calling Abort().
+   //
+   // Use fStatus to set the return value of TTree::Process().
+   //
+   // The return value is currently not used.
+   fReader.SetLocalEntry(entry);
+
+   //Loop over all HSCP candidates
+   //Use the size of any array such as Pt
+   //int iCand = -1; // index on the selected candidate per event
+   //cout<<"TOTO"<<endl;
+   vector<int> iCand(selLabels_.size(),-1);
+   vector<int> maxIh(selLabels_.size(),-1);
+   for(unsigned int i=0;i<Pt.GetSize();i++){
+      //cout<<"pt = "<<Pt[i]<<endl;
+      //Add HSCP selection
+      
+      //if(PassPreselection(i))
+      //if(passPreselection->at(i))
+      //loop over all selection
+      //cout<<"sel: "<<selections_.size()<<endl;
+      for(int s=0;s<selections_.size();s++){
+         //if(selections_[s](i)){
+         bool (HSCPSelector::*ptr)(int);
+	 ptr = selections_[s];
+	 //if(selections_[s](i)){
+	 if((this->*ptr)(i)){
+	   vmrp[s].fill(eta[i],NOM[i],1./(Pt[i]*cosh(eta[i])),Pt[i],PtErr[i],Ih_noL1[i],Ias_StripOnly[i],-1,GetMass(Pt[i]*cosh(eta[i]),Ih_noL1[i],K,C),TOF[i],*nofVtx.Get(),1); 
+            ///*
+	    if(Ih_noL1[i]>maxIh[s]){
+                maxIh[s]=Ih_noL1[i];
+                iCand[s]=i;
+            }
+	    //*/
+         }
+      }    
+   }
+   //End of loop over all HSCP candidates
+
+   // Plots for the candidate per event
+   //if(iCand>=0){
+     
+   //}
+
+   return kTRUE;
+}
+
+void HSCPSelector::SlaveTerminate()
+{
+   // The SlaveTerminate() function is called after all entries or objects
+   // have been processed. When running with PROOF SlaveTerminate() is called
+   // on each slave server.
+   // All plots should be added to the list fOutput to be later on merged
+   //mrp->addToList(fOutput);
+   //Add plots for all MassRegionPlots
+   for(auto obj: vmrp) obj.addToList(fOutput);
+}
+
+void HSCPSelector::Terminate()
+{
+   // The Terminate() function is the last function to be called during
+   // a query. It always runs on the client, it can be used to present
+   // the results graphically or save the results to file.
+   //calling Print on fOutput create an error - don't do that !
+   //fOutput->Print();
+   //create the output file
+   fout = new TFile("out.root","RECREATE");
+   cout<<"Numbers of objects to be stored: "<<fOutput->GetSize()<<endl;
+   //loop over all elements stored
+   TIter next(fOutput);
+   TObject* object = 0;
+   while ((object = next())){
+   	     object->Write();
+   }
+   //TH1F* h = dynamic_cast<TH1F *>(fOutput->FindObject("massFromTreetoto"));
+   //write and close the output file
+   fout->Write();
+   fout->Close();
+}
