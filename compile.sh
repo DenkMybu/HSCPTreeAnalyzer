512944f752d4dd51d0217793edd4339334076cdd
echabert <Eric.Chabert@cern.ch>
2023-04-06 12:08:17 +0200

    update

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/RegionMassPlot.cc 
 g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/PlotTools.cc 
 g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/MassTools.cc 
-g++ -fPIC -shared MassTools.o PlotTools.o RegionMassPlot.o -o libTools.so
+g++ -std=c++11 -fPIC `root-config --cflags --glibs` -c src/CPlots.cc 
+g++ -fPIC -shared CPlots.o MassTools.o PlotTools.o RegionMassPlot.o -o libTools.so
+rm *.o
